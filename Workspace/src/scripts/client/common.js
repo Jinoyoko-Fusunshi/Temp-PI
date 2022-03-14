5f368dba295893d01bae81dbddeea1d9c6aec91a
Jinoyoko Fusunshi <retrogamer2112a@gmail.com>
2022-03-15 00:10:09 +0100

    added common script for needed definitions in other scripts

---
+++ Workspace/src/scripts/client/common.js
@@ -0,0 +1,24 @@
+// Enum for all current needed HTTP Status codes.
+export const HTTPStatusCodes = {
+    Success: 200,
+    NotFound: 404,
+};
+
+// Enum for all current needed HTTP Methods
+export const HTTPMethods = {
+    Get: "GET"
+}
+
+export const BaseURL = "http://localhost:3001";
+
+// All HTTP content types which are needed to write into the head of a HTTP request.
+export const ContentTypes = {
+    HTML: 'text/html',
+    CSS: 'text/css',
+    JS: 'text/javascript',
+    JSON: 'application/json',
+    PNG: 'image/png',
+    JPG: 'image/jpg',
+    ICON: 'image/png',
+    NONE: '',
+};
